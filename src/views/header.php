70dffb99ce9175100d29de64b88c64950c72ae85
Julie Aymar <62514932+Keir-oriyerdan@users.noreply.github.com>
2023-11-20 15:22:36 +0100

    modif

--- src/views/header.php
+++ src/views/header.php
@@ -1,10 +1,10 @@
 <header>
     <nav>
         <ul>
-            <li><a href="http://localhost/PHP_GestionProjet/?controller=UtilisateurController&method=displayUtilisateurs">Utilisateurs</a></li>
-            <li><a href="http://localhost/PHP_GestionProjet/?controller=EditeursController&method=displayEditeurs">Editeurs</a></li>
-            <li><a href="http://localhost/PHP_GestionProjet/?controller=LivresController&method=displayAjoutLivre">Ajouter livre</a></li>
-            <li><a href="http://localhost/PHP_GestionProjet/?controller=EmpruntController&method=displayEmprunts">Emprunts</a></li>
+            <li><a href="?controller=UtilisateurController&method=displayUtilisateurs">Utilisateurs</a></li>
+            <li><a href="?controller=EditeursController&method=displayEditeurs">Editeurs</a></li>
+            <li><a href="?controller=LivresController&method=displayAjoutLivre">Ajouter livre</a></li>
+            <li><a href="?controller=EmpruntController&method=displayEmprunts">Emprunts</a></li>
         </ul>
     </nav>
 </header>
