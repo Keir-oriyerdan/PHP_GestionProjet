42e9071a8d1092632bb7c21ff4e58d36a1313a44
Somenae <kevindevaux@neuf.fr>
2023-11-20 11:35:25 +0100

    premier affichage

---
+++ src/views/header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="http://localhost/phpobjet/mvc/?controller=LivresController&method=displayLivres">Livres</a></li>
+            <li><a href="http://localhost/phpobjet/mvc/?controller=EditeursController&method=displayEditeurs">Editeurs</a></li>
+            <li><a href="http://localhost/phpobjet/mvc/?controller=LivresController&method=displayAjoutLivre">Ajouter livre</a></li>
+            <li><a href="http://localhost/phpobjet/mvc/?controller=EmpruntController&method=displayEmprunts">Emprunts</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ src/views/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        include_once(__DIR__.'/header.php');
+        ?>
+    </body> 
+</html>
