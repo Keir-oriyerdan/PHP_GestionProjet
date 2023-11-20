c0035fb6fe59aae06907ac009b28d7cf75e63afb
Julie Aymar <62514932+Keir-oriyerdan@users.noreply.github.com>
2023-11-20 11:46:25 +0100

    add getter setter sur admin et user

--- src/entity/Administrateur.php
+++ src/entity/Administrateur.php
@@ -4,4 +4,24 @@ namespace Madmax\Skrrr\entity;
 
 class Administrateur{
     private int $id_Admin;
+
+    /**
+     * Get the value of id_Admin
+     */ 
+    public function getId_Admin()
+    {
+        return $this->id_Admin;
+    }
+
+    /**
+     * Set the value of id_Admin
+     *
+     * @return  self
+     */ 
+    public function setId_Admin($id_Admin)
+    {
+        $this->id_Admin = $id_Admin;
+
+        return $this;
+    }
 }
--- src/entity/utilisateur.php
+++ src/entity/utilisateur.php
@@ -4,4 +4,24 @@ namespace Madmax\Skrrr\entity;
 
 class Utilisateur{
     private int $id_utilisateur;
+
+    /**
+     * Get the value of id_utilisateur
+     */ 
+    public function getId_utilisateur()
+    {
+        return $this->id_utilisateur;
+    }
+
+    /**
+     * Set the value of id_utilisateur
+     *
+     * @return  self
+     */ 
+    public function setId_utilisateur($id_utilisateur)
+    {
+        $this->id_utilisateur = $id_utilisateur;
+
+        return $this;
+    }
 }
