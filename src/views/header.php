8c69f42419ed3c8760b45333cdc6f037fdd34d46
louis <louisbousquet13@gmail.com>
2023-11-22 17:27:14 +0100

    css de la mort

--- src/views/header.php
+++ src/views/header.php
@@ -7,15 +7,16 @@ use Madmax\Skrrr\app\SecurityController;
 ?>
 <header>
     <nav>
+        <img src="./img/logo.png" alt="">
         <ul>
+        <li><a href="?controller=EmpruntController&method=displayEmprunts">Emprunts</a></li>
         <?php if (SecurityController::isConnected()) : ?>
             <li><a href="?controller=UtilisateurController&method=ajoutUtilisateur">Ajouter utilisateur</a></li>
             <li><a href="?controller=UtilisateurController&method=displayUtilisateurs">Utilisateurs</a></li>
             <li><a href="?controller=ProjetController&method=displayProjets">Voir les projets</a></li>
+            <li><a href="?controller=SecurityController&method=deconnexion">Déconnexion</a></li>
             <?php else : echo "Vous devez être connecté";?>
             <?php endif; ?>
-            <li><a href="?controller=EmpruntController&method=displayEmprunts">Emprunts</a></li>
-            <li><a href="?controller=SecurityController&method=deconnexion">Déconnexion</a></li>
         </ul>
     </nav>
 </header>
