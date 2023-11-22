3638bbeb000d20b5a73860f39fb8fd05c7ad94f5
Julie Aymar <62514932+Keir-oriyerdan@users.noreply.github.com>
2023-11-22 14:51:53 +0100

    securisation améliorée

--- src/views/header.php
+++ src/views/header.php
@@ -11,11 +11,12 @@ use Madmax\Skrrr\app\SecurityController;
         <?php if (SecurityController::isConnected()) : ?>
             <?php echo "Bienvenue sur le site !"?>
             <li><a href="?controller=SecurityController&method=deconnexion">Déconnexion</a></li>
+            <?php else : echo "Vous devez être connecté";?>
+            <?php endif; ?>
             <li><a href="?controller=UtilisateurController&method=ajoutUtilisateur">Ajouter utilisateur</a></li>
             <li><a href="?controller=UtilisateurController&method=displayUtilisateurs">Utilisateurs</a></li>
             <li><a href="?controller=ProjetController&method=displayProjets">Voir les projets</a></li>
-            <?php else : echo "Vous devez être connecté";?>
-            <?php endif; ?>
+           
             <li><a href="?controller=EmpruntController&method=displayEmprunts">Emprunts</a></li>
         </ul>
     </nav>
