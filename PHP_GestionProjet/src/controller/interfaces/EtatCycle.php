d8d2623cc171be3d14730def0a66ac19cf95aa8b
Louis <113990662+LouisB10@users.noreply.github.com>
2023-11-22 09:45:02 +0100

    Delete PHP_GestionProjet directory

--- PHP_GestionProjet/src/controller/interfaces/EtatCycle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Madmax\Skrrr\controller\interfaces;
-
-interface EtatCycle {
-    public function EtatNonDebute():EtatCycle;
-    public function EtatEnCours():EtatCycle;
-    public function EtatTermine():EtatCycle;
-}
