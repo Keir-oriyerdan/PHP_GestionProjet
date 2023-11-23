e215aba915c3127ec287d049b542c75aa073adcd
Julie Aymar <62514932+Keir-oriyerdan@users.noreply.github.com>
2023-11-23 19:19:48 +0100

    modif code, ajout elements + commentaires du code

--- src/controller/AdministrateurController.php
+++ src/controller/AdministrateurController.php
@@ -8,23 +8,35 @@ use Madmax\Skrrr\app\Model;
 
 class AdministrateurController extends AbstractController {
 
+    // Fonction pour définir un user comme admin
     public function setAdmin()
     {
+        // Création d'un tableau de données avec l'ID de l'user à définir comme admin.
         $datas = [
             'ID_Utilisateur' => $_GET['id'],
         ];
+        // Appel de la fonction pour enregistrer les données dans la table 'administrateur'
         Model::getInstance()->save('administrateur', $datas);
     }
 
+    // Fonction pour supprimer le statut d'admin d'un user
     public function deleteAdmin()
     {
+        // Appel de la fonction pour supprimer un enregistrement d'admin par ID
         Model::getInstance()->deleteById('administrateur', $_GET['id']);
+        
+        // Création d'une instance de IndexController. 
         $index = new IndexController();
+        // appel de sa méthode index.
         $index->index();
     }
 
+    // Fonction pour afficher tous les administrateurs
     public function displayAdmins()
     {
+        // Appel de la fonction pour récupérer et afficher tous les administrateurs
         Model::getInstance()->readAll('administrateur');
     }
+
 }
+
