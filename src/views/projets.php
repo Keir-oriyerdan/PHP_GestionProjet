d1677cc563ddd0f8caeb804538f2559be0bac2ff
Somenae <kevindevaux@neuf.fr>
2023-11-23 09:34:01 +0100

    Résolution liens pour mac

--- src/views/projets.php
+++ src/views/projets.php
@@ -13,7 +13,7 @@
 <body>
     <header>
     <?php 
-    echo '<li><a href=http://localhost/PHP_GestionProjet/>Accueil</a></li>';
+    echo '<li><a href=?controller=IndexController&method=index>Accueil</a></li>';
     ?>
     </header>
     <section>
@@ -22,13 +22,13 @@
     foreach ($projets as $key => $projet) {
         echo '<li class="projectlist">
         Projets: 
-        <a href=http://localhost/PHP_GestionProjet/?controller=ProjetController&method=displayProjet&id='.$projet->getID().'>'.$projet->getNom().'</a>
+        <a href=?controller=ProjetController&method=displayProjet&id='.$projet->getID().'>'.$projet->getNom().'</a>
         <a href=?controller=ProjetController&method=updateProjet&id='.$projet->getID().'>Modifier</a>  
         <a href=?controller=ProjetController&method=supprimerProjet&id='.$projet->getID().'>Supprimer</a>
         </li>';
     }
     ?>
-    <a href="http://localhost/PHP_GestionProjet/?controller=ProjetController&method=ProjetForm"><button type="button">Crée un Projet</button></a>
+    <a href="?controller=ProjetController&method=ProjetForm"><button type="button">Crée un Projet</button></a>
     </section>
 </body>
 </html>
