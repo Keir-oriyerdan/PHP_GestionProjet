8d15fc4dbd79d2685baedbf6a20add96cfe4b3f8
Julie Aymar <62514932+Keir-oriyerdan@users.noreply.github.com>
2023-11-24 12:48:45 +0100

    ajout fonctionalité tache

--- src/controller/TacheController.php
+++ src/controller/TacheController.php
@@ -1,5 +1,5 @@
 <?php 
-/*
+
 //faire en sorte que sur les taches on puisse asigner des etats qu'on retrouvera dans cycle controller
 namespace Madmax\Skrrr\controller;
 
@@ -7,7 +7,47 @@ use Madmax\Skrrr\app\AbstractController;
 use Madmax\Skrrr\controller\interfaces\EtatCycle;
 use Madmax\Skrrr\controller\interfaces\CycleNonDebute;
 use Madmax\Skrrr\app\Model;
+use Madmax\Skrrr\entity\tache;
 
+namespace Madmax\Skrrr\controller;
+
+use Madmax\Skrrr\app\AbstractController;
+use Madmax\Skrrr\app\Model;
+use Madmax\Skrrr\entity\Tache;
+
+// Contrôleur de tâche
+class TacheController extends AbstractController {
+
+    public function displayTache() {
+        $result = Model::getInstance()->getById('tache', $_GET['id']);
+
+       
+        $tache = new Tache();
+        
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['etat_tache'])) {
+            // Modif l'état de la tâche.
+            switch ($_POST['etat_tache']) {
+                case 'non_debute':
+                    $tache->setId(1); // ID pour l'état "Non débuté"
+                    break;
+                case 'en_cours':
+                    $tache->setId(2); // ID pour l'état "En cours"
+                    break;
+                case 'termine':
+                    $tache->setId(3); // ID pour l'état "Terminé"
+                    break;
+                default:
+                    // Gérer les erreurs ou fournir un état par défaut
+                    break;
+            }
+        }
+
+        $this->render('projet.php', ['projet' => $result, 'etatTache' => $tache->afficherEtat()]);
+
+    }
+}
+
+/*
 class TacheController extends AbstractController {
 
     /* private EtatCycle $etattache;
@@ -30,58 +70,3 @@ class TacheController extends AbstractController {
 }
 */
 
-namespace Madmax\Skrrr\controller;
-
-use Madmax\Skrrr\app\AbstractController;
-use Madmax\Skrrr\app\Model;
-
-// Interface pour définir les états de cycle
-interface EtatCycle {
-    public function afficherEtat();
-}
-
-// Classe abstraite pour représenter une tâche
-abstract class Tache implements EtatCycle {
-    protected $etat;
-
-    public function __construct(EtatCycle $etat) {
-        $this->etat = $etat;
-    }
-
-    public function afficherEtat() {
-        return $this->etat->afficherEtat();
-    }
-}
-
-// Classe concrète pour l'état non débuté
-class CycleNonDebute implements EtatCycle {
-    public function afficherEtat() {
-        return "Non débuté";
-    }
-}
-
-// Classe concrète pour l'état en cours
-class CycleEnCours implements EtatCycle {
-    public function afficherEtat() {
-        return "En cours";
-    }
-}
-
-// Classe concrète pour l'état terminé
-class CycleTermine implements EtatCycle {
-    public function afficherEtat() {
-        return "Terminé";
-    }
-}
-
-// Contrôleur de tâche
-class TacheController extends AbstractController {
-    public function displayTache() {
-        $result = Model::getInstance()->getById('tache', $_GET['id']);
-
-        // Utilisation de la classe Tache avec l'état non débuté par défaut
-        $tache = new Tache(new CycleNonDebute());
-
-        $this->render('projet.php', ['projet' => $result, 'etatTache' => $tache->afficherEtat()]);
-    }
-}
--- src/entity/tache.php
+++ src/entity/tache.php
@@ -2,152 +2,51 @@
 
 namespace Madmax\Skrrr\entity;
 
-class tache {
+class Tache {
     private ?int $ID;
     private ?string $Titre;
     private ?string $Description;
     private ?int $Id_Priorite;
-    private ?int $ID_Cycle;
-    private ?int $ID_Utilisateur;
-    private ?int $ID_Projet;
+    private ?int $id;
+
+    // Autres méthodes et propriétés
 
     /**
-     * Get the value of ID
+     * Get the value of Id
      */ 
-    public function getID()
+    public function getId()
     {
-        return $this->ID;
+        return $this->id;
     }
 
     /**
-     * Set the value of ID
+     * Set the value of Id
      *
      * @return  self
      */ 
-    public function setID($ID)
+    public function setId($id)
     {
-        $this->ID = $ID;
+        $this->id = $id;
 
         return $this;
     }
 
     /**
-     * Get the value of Titre
-     */ 
-    public function getTitre()
-    {
-        return $this->Titre;
+     * Affiche l'état de la tâche
+     */
+    public function afficherEtat() {
+        // Vous pouvez implémenter la logique pour afficher l'état en fonction de l'ID
+        switch ($this->id) {
+            case 1:
+                return "Non débuté";
+            case 2:
+                return "En cours";
+            case 3:
+                return "Terminé";
+            default:
+                return "État inconnu";
+        }
     }
 
-    /**
-     * Set the value of Titre
-     *
-     * @return  self
-     */ 
-    public function setTitre($Titre)
-    {
-        $this->Titre = $Titre;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Description
-     */ 
-    public function getDescription()
-    {
-        return $this->Description;
-    }
-
-    /**
-     * Set the value of Description
-     *
-     * @return  self
-     */ 
-    public function setDescription($Description)
-    {
-        $this->Description = $Description;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Id_Priorite
-     */ 
-    public function getId_Priorite()
-    {
-        return $this->Id_Priorite;
-    }
-
-    /**
-     * Set the value of Id_Priorite
-     *
-     * @return  self
-     */ 
-    public function setId_Priorite($Id_Priorite)
-    {
-        $this->Id_Priorite = $Id_Priorite;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of ID_Cycle
-     */ 
-    public function getID_Cycle()
-    {
-        return $this->ID_Cycle;
-    }
-
-    /**
-     * Set the value of ID_Cycle
-     *
-     * @return  self
-     */ 
-    public function setID_Cycle($ID_Cycle)
-    {
-        $this->ID_Cycle = $ID_Cycle;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of ID_Utilisateur
-     */ 
-    public function getID_Utilisateur()
-    {
-        return $this->ID_Utilisateur;
-    }
-
-    /**
-     * Set the value of ID_Utilisateur
-     *
-     * @return  self
-     */ 
-    public function setID_Utilisateur($ID_Utilisateur)
-    {
-        $this->ID_Utilisateur = $ID_Utilisateur;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of ID_Projet
-     */ 
-    public function getID_Projet()
-    {
-        return $this->ID_Projet;
-    }
-
-    /**
-     * Set the value of ID_Projet
-     *
-     * @return  self
-     */ 
-    public function setID_Projet($ID_Projet)
-    {
-        $this->ID_Projet = $ID_Projet;
-
-        return $this;
-    }
+    // Autres méthodes et propriétés
 }
